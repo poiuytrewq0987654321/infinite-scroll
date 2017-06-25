0650f8ce525a6b86b63537deabcda0ba7c58031d
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-06-25 16:25:07 +0200

    infinite scroll - AJAX + jQuery

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+/* pobieranie danych ze strony przy pomocy AJAX i wyświetlenie ich na własnej stronie w odpowiedniej formie */
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            $.getJSON('https://jsonplaceholder.typicode.com/users', function (data) {
+                console.log(data);
+                
+                data.forEach(function(element){
+                    console.log(element.name);
+                    console.log(element.id);
+                    console.log(element.website);
+                    $('body').append('<p>ID użytkownika to: ' + element.id + '</p>');
+                    $('body').append('<p>nazwa użytkownika to: ' + element.name+ '</p>');
+                    $('body').append('<p>url użytkownika to: ' + element.website + '</p>');
+                    $('body').append('<hr>');
+                    
+                });
+            });
+        }
+    });
+});
